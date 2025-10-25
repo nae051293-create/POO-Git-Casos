acd9dc1d6fdd04502f31c86d0ccde80ec3bcf828
nae051293-create <nae051293@gmail.com>
2025-10-24 22:44:58 -0500

    Caso 1: se crea programa que solicita datos con Scanner

--- src/caso1/UsuarioSimple.java
+++ src/caso1/UsuarioSimple.java
@@ -1,10 +1,21 @@
 package caso1;
 
+import java.util.Scanner;
+
 public class UsuarioSimple {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ingrese su nombre: ");
+        String nombre = sc.nextLine();
+
+        System.out.print("Ingrese su edad: ");
+        int edad = sc.nextInt();
+
+        System.out.println("Hola " + nombre + ", tienes " + edad + " años.");
+        sc.close();
 	}
 
 }
